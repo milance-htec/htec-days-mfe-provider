55974db5e12da3b57555fb11ccdd47302b164b00
micke95 <milan.milicevic@htecgroup.com>
2020-12-27 17:22:21 +0100

    chore: remove common folder

--- src/common/constants.ts
+++
@@ -1,8 +0,0 @@
-import { UserModulePermission } from "./types";
-
-export const USER_MODULE_PERIMISSION: {
-  [K in UserModulePermission]: UserModulePermission;
-} = {
-  OWNER: "OWNER",
-  VIEWER: "VIEWER",
-};
--- src/common/types.ts
+++
@@ -1,8 +0,0 @@
-export type UserModuleMap = { [moduleName: string]: UserModulePermission };
-
-export type UserModulePermission = "OWNER" | "VIEWER";
-
-export type UserModule = {
-  moduleName: string;
-  permission: UserModulePermission;
-};
